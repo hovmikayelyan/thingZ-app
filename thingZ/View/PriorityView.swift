a9c5b04c5ad214654b7453660e25ee6b0e80906a
Hovhannes Mikayelyan <hovmikayelyan@gmail.com>
2022-06-13 14:26:19 +0400

    Initial commit

---
+++ thingZ/View/PriorityView.swift
@@ -0,0 +1,21 @@
+//
+//  PriorityView.swift
+//  thingZ
+//
+//  Created by Hovhannes Mikayelyan on 6/13/22.
+//  Copyright © 2022 Hovhannes Mikayelyan. All rights reserved.
+//
+
+import UIKit
+
+class PriorityView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ thingZ/View/TaskTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  TaskTxtField.swift
+//  thingZ
+//
+//  Created by Hovhannes Mikayelyan on 6/13/22.
+//  Copyright © 2022 Hovhannes Mikayelyan. All rights reserved.
+//
+
+import UIKit
+
+class TaskTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
