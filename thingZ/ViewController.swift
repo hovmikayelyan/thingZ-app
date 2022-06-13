d90bc795f84235c2deafc3f486f9aaaf7ab30992
Hovhannes Mikayelyan <hovmikayelyan@gmail.com>
2022-06-13 10:09:11 +0400

    Initial Commit

---
+++ thingZ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thingZ
+//
+//  Created by Hovhannes Mikayelyan on 6/13/22.
+//  Copyright © 2022 Hovhannes Mikayelyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
