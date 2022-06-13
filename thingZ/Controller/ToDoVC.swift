c62be16b77e1d4cdc4503b02f78af2acae172f60
Hovhannes Mikayelyan <hovmikayelyan@gmail.com>
2022-06-13 17:19:41 +0400

    Fix storyboard and add new features

--- thingZ/Controller/ToDoVC.swift
+++ thingZ/Controller/ToDoVC.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ToDoVC: UIViewController {
 
-
+    @IBOutlet weak var tasksTable: UITableView!
+    @IBOutlet weak var addTaskBtn: UIButton!
     @IBOutlet weak var newTaskView: UIView!
     @IBOutlet weak var taskTxtField: TaskTxtField!
+    @IBOutlet weak var prioritySegment: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        taskTxtField.inputAccessoryView = newTaskView
+        self.bindToKeyboard()
     }
-
-
+    
+    @IBAction func addTaskBtnWasPressed(_ sender: Any) {
+    }
+    
 }
 
+
+extension ToDoVC {
+
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let startingFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endingFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endingFrame.origin.y - startingFrame.origin.y
+
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.view.frame.origin.y += deltaY
+        }, completion: nil)
+
+    }
+
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+}
---
+++ thingZ/View/TodoCell.swift
@@ -0,0 +1,27 @@
+//
+//  TodoCell.swift
+//  thingZ
+//
+//  Created by Hovhannes Mikayelyan on 6/13/22.
+//  Copyright © 2022 Hovhannes Mikayelyan. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    @IBOutlet weak var itemLbl: UILabel!
+    @IBOutlet weak var priorityView: PriorityView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
