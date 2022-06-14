65317e06d278931bed6c5f9514f4d3ebc448b154
Hovhannes Mikayelyan <hovmikayelyan@gmail.com>
2022-06-14 13:40:21 +0400

    Modify NetworkService.swift, optimize functions

--- thingZ/Services/NetworkService.swift
+++ thingZ/Services/NetworkService.swift
@@ -11,66 +11,66 @@ import Foundation
 class NetworkService {
     static let shared = NetworkService()
     
-    let URL_BASE = "http://localhost:3003"
-    let URL_ADD_TOTO = "/add"
-    
-    let session = URLSession(configuration: .default)
-    
     func getTodos(_ onSuccess: @escaping (Todos) -> Void, onError: @escaping (String) -> Void) {
-        let url = URL(string: "\(URL_BASE)")!
+        let process = fetchData(type: "get")
+        process.work(todo: nil, onSuccess: onSuccess, onError: onError)
+    }
+    
+    func addTodo(todo: Todo, onSuccess: @escaping (Todos) -> Void, onError: @escaping (String) -> Void) {
+        let process = fetchData(type: "add")
+        process.work(todo: todo, onSuccess: onSuccess, onError: onError)
+    }
+}
+
+extension NetworkService {
+    struct fetchData {
         
-        let task = session.dataTask(with: url) { (data, response, error) in
+        let type: String
+        let URL_BASE = "http://localhost:3003"
+        let URL_ADD_TOTO = "/add"
+        
+        let session = URLSession(configuration: .default)
+        
+        init(type:  String) {
+            self.type = type
+        }
+        
+        func work(todo: Todo?, onSuccess: @escaping (Todos) -> Void, onError: @escaping (String) -> Void){
             
-            DispatchQueue.main.async {
-                if let error = error {
-                    onError(error.localizedDescription)
-                    return
-                }
-                
-                guard let data = data, let response = response as? HTTPURLResponse else {
-                    onError("Invalid data or response")
-                    return
-                }
+            var request: URLRequest
+            
+            if type == "add"
+            {
+                let url = URL(string: "\(URL_BASE)\(URL_ADD_TOTO)")!
+                request = URLRequest(url : url)
+                request.httpMethod = "POST"
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.addValue("application/json", forHTTPHeaderField: "Accept")
                 
                 do {
-                    if response.statusCode == 200 {
-                        let items = try JSONDecoder().decode(Todos.self, from: data)
-                        onSuccess(items)
-                    }
-                    else {
-                        let err = try JSONDecoder().decode(APIError.self, from: data)
-                        onError(err.message)
-                    }
+                    let body = try JSONEncoder().encode(todo!)
+                    request.httpBody = body
                 } catch {
                     onError(error.localizedDescription)
                 }
             }
-        }
-        task.resume()
-    }
-    
-    func addTodo(todo: Todo, onSuccess: @escaping (Todos) -> Void, onError: @escaping (String) -> Void) {
-        let url = URL(string: "\(URL_BASE)\(URL_ADD_TOTO)")!
-        var request = URLRequest(url : url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        do {
-            let body = try JSONEncoder().encode(todo)
-            request.httpBody = body
+            else
+            {
+                let url = URL(string: "\(URL_BASE)")!
+                request = URLRequest(url: url)
+                request.httpMethod = "GET"
+            }
             
             let task = session.dataTask(with: request) { (data, response, error) in
                 
                 DispatchQueue.main.async {
-                    
                     if let error = error {
                         onError(error.localizedDescription)
                         return
                     }
                     
                     guard let data = data, let response = response as? HTTPURLResponse else {
-                        onError("Failed to get data from server.")
+                        onError("Invalid data or response. Failed to get data from server.")
                         return
                     }
                     
@@ -89,8 +89,7 @@ class NetworkService {
                 }
             }
             task.resume()
-        } catch {
-             onError(error.localizedDescription)
+            
         }
     }
 }
